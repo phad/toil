0c9a5c9ddefa10d7fdda853e06f4b09d27c41f26
Paul Hadfield <paul@phad.org.uk>
2014-11-04 14:57:49 +0000

    80-col

--- spec/tokeniser_spec.rb
+++ spec/tokeniser_spec.rb
@@ -20,15 +20,18 @@ describe Toil::Tokeniser do
     end
 
     it 'emits a single whitespace token for " "' do
-      expect(@subject.tokenise(' ')).to eq [{:type => :whitespace, :content => " "}]
+      expect(@subject.tokenise(' ')).to eq [{:type => :whitespace,
+                                             :content => " "}]
     end
 
     it 'emits a single whitespace token for "\t"' do
-      expect(@subject.tokenise("\t")).to eq [{:type => :whitespace, :content => "\t"}]
+      expect(@subject.tokenise("\t")).to eq [{:type => :whitespace,
+                                              :content => "\t"}]
     end
 
     it 'emits a single whitespace token for multiple " "' do
-      expect(@subject.tokenise('   ')).to eq [{:type => :whitespace, :content => "   "}]
+      expect(@subject.tokenise('   ')).to eq [{:type => :whitespace,
+                                               :content => "   "}]
     end
 
     it 'emits a single symbol token for "a"' do
@@ -36,23 +39,32 @@ describe Toil::Tokeniser do
     end
 
     it 'emits a single symbol token for "ab"' do
-      expect(@subject.tokenise('ab')).to eq [{:type => :symbol, :content => "ab"}]
+      expect(@subject.tokenise('ab')).to eq [{:type => :symbol,
+                                              :content => "ab"}]
     end
 
     it 'emits two sexpr tokens for "()"' do
-      expect(@subject.tokenise('()')).to eq [{:type => :sexpr_start}, {:type => :sexpr_end}]
+      expect(@subject.tokenise('()')).to eq [{:type => :sexpr_start},
+                                             {:type => :sexpr_end}]
     end
 
     it 'emits five tokens for " ( ) "' do
-      types = @subject.tokenise(' ( ) ').map{|tok| tok[:type]}
-      expect(types).to eq [:whitespace, :sexpr_start, :whitespace, :sexpr_end, :whitespace]
+      tokens = @subject.tokenise(' ( ) ')
+      types = tokens.map{|tok| tok[:type]}
+      contents = tokens.map{|tok| tok[:content]}
+
+      expect(types).to eq [:whitespace, :sexpr_start, :whitespace,
+                           :sexpr_end, :whitespace]
+      expect(contents).to eq [' ', nil, ' ', nil, ' ']
     end
 
     it 'emits five tokens for "(a b)"' do
       tokens = @subject.tokenise('(a b)')
       types = tokens.map{|tok| tok[:type]}
       contents = tokens.map{|tok| tok[:content]}
-      expect(types).to eq [:sexpr_start, :symbol, :whitespace, :symbol, :sexpr_end]
+
+      expect(types).to eq [:sexpr_start, :symbol, :whitespace, :symbol,
+                           :sexpr_end]
       expect(contents).to eq [nil, 'a', ' ', 'b', nil]
     end
 
@@ -60,10 +72,11 @@ describe Toil::Tokeniser do
       tokens = @subject.tokenise('(foo bar baz)')
       types = tokens.map{|tok| tok[:type]}
       contents = tokens.map{|tok| tok[:content]}
-      expect(types).to eq [:sexpr_start, :symbol, :whitespace, :symbol, :whitespace, :symbol, :sexpr_end]
+
+      expect(types).to eq [:sexpr_start, :symbol, :whitespace, :symbol,
+                           :whitespace, :symbol, :sexpr_end]
       expect(contents).to eq [nil, 'foo', ' ', 'bar', ' ', 'baz', nil]
     end
-
   end
 
 end
